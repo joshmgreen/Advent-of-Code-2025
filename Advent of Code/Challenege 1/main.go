3ecdfbf6010434143f2d1f6a99177a751bc9803e
joshmgreen <joshx3g@gmail.com>
2025-12-03 14:35:09 -0600

    day 1 part 1 solution

--- Advent of Code/Challenege 1/main.go
+++ Advent of Code/Challenege 1/main.go
@@ -1,60 +1,45 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
 
-	file, err := os.Open("input.txt")
+	file, err := os.ReadFile("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
 
-	// Read file into a slice of strings
-	var lines []string
+	lines := strings.Fields(string(file))
 
-	// Create scanner for file
-	scanner := bufio.NewScanner(file)
-
-	// Loop over input file and store in slice
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	// Close the file
-	file.Close()
-
-	// zeroCount := 0
-	direction := ""
-	rotations := 0
-	startingPosition := 50
+	currentPosition := 50
 	zeroCount := 0
 
 	// Loop over input
 	for _, value := range lines {
-		direction = value[:1]
-		rotations, err = strconv.Atoi(value[1:])
+
+		direction := string(value[0])
+		rotations, err := strconv.Atoi(strings.TrimSpace(value[1:]))
+
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+
 		if direction == "L" {
-			startingPosition = startingPosition - rotations
-		} else {
-			startingPosition = startingPosition + rotations
+			currentPosition = ((currentPosition - rotations)%100 + 100) % 100
 		}
-		if startingPosition == 0 {
+
+		if direction == "R" {
+			currentPosition = (currentPosition + rotations) % 100
+		}
+
+		if currentPosition == 0 {
 			zeroCount++
-		}
-		if startingPosition > 99 {
-			difference := startingPosition - 100
-			startingPosition = startingPosition - difference
-		}
-		if startingPosition < 0 {
-			difference := startingPosition + 100
-			startingPosition = startingPosition + difference
 		}
 	} 
 	fmt.Println(zeroCount)
